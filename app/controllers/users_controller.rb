13fd09b584e71d9f8627d5c9f0c3e260c3dac3f1
Heri Susanto <susantoh41@gmail.com>
2018-10-02 16:31:54 +0700

    Ready for production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_same_user, only: [:edit, :update]
-
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   # GET /users
   # GET /users.json
   def index
@@ -51,11 +51,10 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
   end
 
   private
@@ -69,8 +68,14 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :email, :password)
     end
     def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
+    end
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "Only admin users can perform that action"
         redirect_to root_path
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_074943) do
+ActiveRecord::Schema.define(version: 2018_10_02_084651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,22 +22,12 @@ ActiveRecord::Schema.define(version: 2018_09_26_074943) do
     t.datetime "updated_at"
     t.integer "user_id"
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "description"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
-
-  add_foreign_key "comments", "users"
 end
