7fcb617e3db7095e3fe62f8c947a5945a7c2427f
Heri Susanto <susantoh41@gmail.com>
2018-10-02 15:44:46 +0700

    Add login upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,11 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    @user = User.new(user_params)
     if @user.save
+        session[:user_id] = @user.id
         flash[:success] = "Welcome to the Heri Blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to user_path(@user)
     else
         render 'new'
     end
